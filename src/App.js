c36d2e86d5a0d1ef02c9a9bdab7ccb3eddc90970
Konrad Bauer <konradbauer94@gmail.com>
2023-03-07 11:51:42 +0100

    import Header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./common/Theme/theme";
+import { Header } from "./Header";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Header />
     </ThemeProvider>
   );
 }
