62cd81021b01532f460ecd4f8306f2b270ec6426
Konrad Bauer <konradbauer94@gmail.com>
2023-03-08 13:25:52 +0100

    add NoResult component

---
+++ src/layouts/NoResult/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Disclaimer = styled.span`
+  margin-top: 50px;
+  margin-left: 276px;
+  width: 674px;
+  height: 43px;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.color.black};
+`;
+
+export const PictureWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
