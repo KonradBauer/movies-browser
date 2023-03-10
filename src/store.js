722295d554f8573987c192b68b26423d9a2b73f6
Konrad Bauer <konradbauer94@gmail.com>
2023-03-10 16:00:44 +0100

    add moviesReducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import moviesReducer from "./features/movies/movie/moviesSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    moviesReducer,
+  },
 });
