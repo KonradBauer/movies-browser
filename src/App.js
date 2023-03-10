f7e53045a7b113601470898e8eb0e2e3060f682d
Konrad Bauer <konradbauer94@gmail.com>
2023-03-10 20:50:02 +0100

    deleted test import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./common/Theme/theme";
-import { BigPoster } from "./layouts/BigPoster";
 import { Header } from "./layouts/Header/index";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      <BigPoster />
     </ThemeProvider>
   );
 }
