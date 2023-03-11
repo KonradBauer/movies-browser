7f0504cd839629e287282ac8cfc9a57031d6abdd
Konrad Bauer <konradbauer94@gmail.com>
2023-03-11 17:08:49 +0100

    Pagination added to App.js for test view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./common/Theme/theme";
 import { Header } from "./layouts/Header/index";
+import { Pagination } from "./layouts/Pagination/index";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <Pagination />
     </ThemeProvider>
   );
 }
