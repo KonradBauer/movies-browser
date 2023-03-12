b1adaeceda03e7a425e6788c068c8dba9a8b9fd9
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-03-12 02:59:27 +0100

    added another person image in details and mobile

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import { theme } from "./common/Theme/theme";
 import { Tile } from "./layouts/Tile";
 import poster from "./common/images/poster.png";
 import person from "./common/images/person.png";
+import personMobileAndDetails from "./common/images/personMobileAndDetails.png";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
-      {/* <Tile movie={"movie"} srcImage={poster} />
-      <Tile movieDetails={"movieDetails"} srcImage={poster} /> */}
-      <Tile person={"person"} role={"Mulan"} srcImage={person} />
-      <Tile person={"person"} role={"Komandor Tung"} srcImage={person} />
-      <Tile person={"person"} srcImage={person} />
+      {/* <Tile movie={"movie"} srcImage={poster} /> */}
+      <Tile movieDetails={"movieDetails"} srcImage={poster} />
+      <Tile
+        person={"person"}
+        role={"Mulan"}
+        srcImage={windowWidth > 830 ? person : personMobileAndDetails}
+      />
+      {/* <Tile person={"person"} srcImage={person} /> */}
+      <Tile personDetails={"personDetails"} srcImage={personMobileAndDetails} />
     </ThemeProvider>
   );
 }
