a63b8bc5a6b647f60038e554bee860422c872d2d
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-03-12 03:07:44 +0100

    deleted unnecessary line

--- src/layouts/Tile/Description/styled.js
+++ src/layouts/Tile/Description/styled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const MovieDescription = styled.div`
   color: ${({ theme }) => theme.color.black};
   font-size: 20px;
-  font-weight: 400;
   line-height: 1.6;
   max-width: inherit;
   grid-area: Description;
