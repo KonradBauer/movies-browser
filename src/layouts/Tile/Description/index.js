210a6f34c5f0bd87f496148923d75f539e59081b
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-03-12 03:06:50 +0100

    code structure in Description changed

--- src/layouts/Tile/Description/index.js
+++ src/layouts/Tile/Description/index.js
@@ -1,11 +1,26 @@
 import { MovieDescription } from "./styled";
 
-export const DescriptionComponent = () => {
+export const DescriptionComponent = ({movieDetails, personDetails}) => {
   return (
-    <MovieDescription>
-      A young Chinese maiden disguises herself as a male warrior in order to save her father.
-      Disguises herself as a male warrior in order to save her father. A young Chinese maiden
-      disguises herself as a male warrior in order to save her father.
-    </MovieDescription>
+    <>
+      {movieDetails ? (
+        <MovieDescription>
+          A young Chinese maiden disguises herself as a male warrior in order to save her father.
+          Disguises herself as a male warrior in order to save her father. A young Chinese maiden
+          disguises herself as a male warrior in order to save her father.
+        </MovieDescription>
+      ) : (
+        ""
+      )}
+      {personDetails ? (
+        <MovieDescription>
+          Liu Yifei was born in Wuhan, Hubei, Province of China on August 25th, 1987. She began
+          modeling at the age of 8 and was trained in singing, dancing and the piano. Moving to the
+          United States at 10 with her mother, Liu lived there for four years.
+        </MovieDescription>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
