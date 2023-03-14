7945a3c3c0226015f11fcf9feb3afff64c8c7f52
Konrad Bauer <konradbauer94@gmail.com>
2023-03-14 20:53:49 +0100

    removed unnecessary things in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./common/Theme/theme";
 import { Header } from "./layouts/Header/index";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <ThemeProvider theme={theme}>
       <Header />
