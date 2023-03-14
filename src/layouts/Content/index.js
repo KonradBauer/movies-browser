a292d22b13c057581ed3fff1f98bbc4ee77dbd50
Konrad Bauer <konradbauer94@gmail.com>
2023-03-14 18:07:00 +0100

    added Tile import to Content component, add Content import to App.js

--- src/layouts/Content/index.js
+++ src/layouts/Content/index.js
@@ -1,9 +1,39 @@
-import { BoxTitle, MainBox } from "./styled";
+import { BoxTitle, MainBox, Section, TilesList } from "./styled";
+import { Tile } from "../Tile/index";
+import poster from "../../common/images/poster.png";
 
 export const Content = () => {
   return (
     <MainBox>
-      <BoxTitle>Popular movies</BoxTitle>
+      <Section>
+        <BoxTitle>Popular movies</BoxTitle>
+        <TilesList>
+          <li>
+            <Tile movie={"movie"} srcImage={poster} />
+          </li>
+          <li>
+            <Tile movie={"movie"} srcImage={poster} />
+          </li>
+          <li>
+            <Tile movie={"movie"} srcImage={poster} />
+          </li>
+          <li>
+            <Tile movie={"movie"} srcImage={poster} />
+          </li>
+          <li>
+            <Tile movie={"movie"} srcImage={poster} />
+          </li>
+          <li>
+            <Tile movie={"movie"} srcImage={poster} />
+          </li>
+          <li>
+            <Tile movie={"movie"} srcImage={poster} />
+          </li>
+          <li>
+            <Tile movie={"movie"} srcImage={poster} />
+          </li>
+        </TilesList>
+      </Section>
     </MainBox>
   );
 };
--- src/layouts/Content/styled.js
+++ src/layouts/Content/styled.js
@@ -1,24 +1,37 @@
 import styled from "styled-components";
 
 export const MainBox = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
   margin: auto;
   margin-top: 56px;
   margin-bottom: 40px;
   width: 1368px;
   height: 1391px;
   border: 1px solid black;
+  background: none;
+  display: flex;
+`;
+
+export const Section = styled.section`
+  display: block;
 `;
 
 export const BoxTitle = styled.span`
   width: 285px;
   height: 43px;
+  margin-bottom: 24px;
   font-style: normal;
   font-weight: 600;
   font-size: 36px;
   line-height: 120%;
-  display: flex;
-  align-items: center;
   color: ${({ theme }) => theme.color.woodsmoke};
+`;
+
+export const TilesList = styled.ul`
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+  display: grid;
+  justify-content: center;
+  gap: 24px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
 `;
