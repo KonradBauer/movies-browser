889ccb31ac472ed80b755931905ce7b39541acde
Konrad Bauer <konradbauer94@gmail.com>
2023-03-19 18:36:32 +0100

    Pagination now reacts at window width and change styles

--- src/layouts/Pagination/index.js
+++ src/layouts/Pagination/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { ReactComponent as VectorLeft } from "../../common/svg/vectorLeft.svg";
 import { ReactComponent as VectorRight } from "../../common/svg/vectorRight.svg";
 import {
@@ -13,12 +14,22 @@ import {
   PageTotal,
 } from "./styled";
 
-export const Pagination = ({
-  ButtonFirstMobile,
-  ButtonPreviousMobile,
-  ButtonNextMobile,
-  ButtonLastMobile,
-}) => {
+export const Pagination = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowWidth < 768 ? <PaginationMobile /> : <PaginationDesktop />;
+};
+
+const PaginationDesktop = () => {
   return (
     <Box>
       <ButtonFirst>
@@ -41,6 +52,33 @@ export const Pagination = ({
       </ButtonNext>
       <ButtonLast>
         Last
+        <VectorRight />
+      </ButtonLast>
+    </Box>
+  );
+};
+
+const PaginationMobile = () => {
+  return (
+    <Box>
+      <ButtonFirst>
+        <VectorLeft />
+        <VectorLeft />
+      </ButtonFirst>
+      <ButtonPrevious>
+        <VectorLeft />
+      </ButtonPrevious>
+      <Pages>
+        <Page>Page</Page>
+        <PageNumber>1</PageNumber>
+        <Of>of</Of>
+        <PageTotal>500</PageTotal>
+      </Pages>
+      <ButtonNext>
+        <VectorRight />
+      </ButtonNext>
+      <ButtonLast>
+        <VectorRight />
         <VectorRight />
       </ButtonLast>
     </Box>
