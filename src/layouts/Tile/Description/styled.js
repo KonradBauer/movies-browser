906947c68ea959c16cdc4a378e633939b4d9efa6
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-03-22 19:43:26 +0100

    added responsivenes movie details Tile

--- src/layouts/Tile/Description/styled.js
+++ src/layouts/Tile/Description/styled.js
@@ -6,10 +6,11 @@ export const DescriptionContent = styled.div`
   line-height: 1.6;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.beforeMobileMax}px) {
-    font-size: 17px;
+    font-size: 18px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    grid-area: 2 / 1 / 3 / 3;
     font-size: 14px;
   }
 
--- src/layouts/Tile/movies/movieDetails/styled.js
+++ src/layouts/Tile/movies/movieDetails/styled.js
@@ -1,69 +1,42 @@
 import styled, { css } from "styled-components";
 
 export const Container = styled.div`
-  position: relative;
-  padding: 16px;
-  border-radius: 5px;
-  margin: 10px auto;
-  max-width: 324px;
-  height: 650px;
-  background: ${({ theme }) => theme.color.white};
-  box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    max-width: 288px;
-    height: 201px;
-    display: flex;
-    gap: 16px;
-  }
-
   ${({ movieDetails }) =>
     movieDetails &&
     css`
-      max-width: 1368px;
-      height: fit-content;
-      max-height: 544px;
-      padding: 40px;
       display: grid;
-      grid-template-columns: 312px 1fr;
-      grid-template-rows: 1fr 1fr;
+      grid-template-columns: repeat(2, auto);
+      grid-template-rows: repeat(2, auto);
       gap: 27px 40px;
-      grid-template-areas:
-        "Poster Information"
-        "Poster Description";
+      margin: 10px auto;
+      padding: 40px;
+      background: ${({ theme }) => theme.color.white};
+      box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
 
       @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-        height: 400px;
+        height: 100%;
         padding: 16px;
-        flex-wrap: wrap;
-      }
-
-      @media (max-width: ${({ theme }) => theme.breakpoints.theSmallestMobileView}px) {
-        min-height: 410px;
+        grid-template-columns: auto 1fr;
+        gap: 16px;
       }
     `}
 `;
 
 export const Image = styled.img.attrs((props) => ({ src: props.source }))`
-  border-radius: 5px;
-  width: 292px;
-  height: 434px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    width: 114px;
-    height: 169px;
-  }
-
   ${({ movieDetails }) =>
     movieDetails &&
     css`
-      width: 312px;
-      height: 464px;
-      grid-area: Poster;
+      border-radius: 5px;
+      max-width: 312px;
+      grid-area: 1 / 1 / 3 / 2;
 
-      @media (max-width: 290px) {
-        width: 105px;
-        height: 157px;
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        max-width: 114px;
+        grid-area: 1 / 1 / 2 / 2;
+      }
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}px) {
+        max-width: 105px;
       }
     `}
 `;
@@ -74,30 +47,18 @@ export const MovieContainer = styled.div`
     css`
       max-width: 936px;
       padding: 8px 0 0;
+      grid-area: 1 / 2 / 2 / 3;
 
       @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         padding: 0;
         display: flex;
         flex-direction: column;
         gap: 8px;
+        grid-area: 1 / 2 / 2 / 3;
       }
     `}
 `;
 
 export const DescriptionContainer = styled.div`
-  margin: 16px 0 39px 0;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    margin: 0 0 8px;
-  }
-
-  ${({ movieDetails }) =>
-    movieDetails &&
-    css`
       margin: 0;
-
-      @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-        margin: 0;
-      }
-    `}
 `;
