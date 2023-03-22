faf5abf93d9f76615fbb54068d416bc65182adc1
Konrad Bauer <konradbauer94@gmail.com>
2023-03-22 22:15:39 +0100

    fix bugs in router, now test buttons switch the page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
-import { HashRouter, Route, Redirect } from "react-router-dom";
+import { HashRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import { MoviesButton, PeopleButton } from "./layouts/Header/styled";
 import MoviesBrowser from "./MoviesBrowser";
+import { PeopleContent } from "./layouts/Contents/index";
 
 export const App = () => {
   return (
     <HashRouter>
-      <Route path="/">
-        <Redirect to="/popular-movies" />
-      </Route>
-      <Route path="/popular-movies">
-        <MoviesBrowser />
-      </Route>
+      <nav>
+        <Link to="/popular-movies">Movies</Link>
+        <Link to="/popular-people">People</Link>
+
+        <Switch>
+          <Route path="/popular-movies">
+            <MoviesBrowser />
+          </Route>
+          <Route path="/popular-people">
+            <PeopleContent />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/popular-movies" />
+          </Route>
+        </Switch>
+      </nav>
     </HashRouter>
   );
 };
