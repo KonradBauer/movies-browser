edabaf6d0f0556f7b1da81f42c4177289bce869f
Konrad Bauer <konradbauer94@gmail.com>
2023-03-23 23:37:56 +0100

    Co-authored-by: Piotr Paczuski <piotrpaczuski@users.noreply.github.com>
    dispatch functions and imports moved to App.js

--- src/MoviesBrowser.js
+++ src/MoviesBrowser.js
@@ -1,21 +1,5 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchMovies } from "./features/movies/movie/moviesSlice";
 import { Content } from "./layouts/Contents/index";
-import { fetchPeople } from "./features/peoples/people/peopleSlice";
-import { fetchGenres } from "./features/movies/genresSilce";
-import { fetchConfiguration } from "./features/configurationSlice";
-
 function MoviesBrowser() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchConfiguration());
-    dispatch(fetchGenres());
-    dispatch(fetchMovies());
-    dispatch(fetchPeople());
-  }, []);
-
   return <Content />;
 }
 
