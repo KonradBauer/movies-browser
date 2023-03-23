e2b9e9eae6eb1d4fdf9963e407b6f23608eee311
Konrad Bauer <konradbauer94@gmail.com>
2023-03-23 23:44:29 +0100

    add loading screens between pages, add dispatch functions and imports

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MoviesBrowser from "./MoviesBrowser";
 import { PeopleContent } from "./layouts/Contents/index";
 import { PersonDetails } from "./layouts/PersonDetails/index";
 import { MoviesDetails } from "./layouts/MoviesDetails/index";
+import { Loading } from "./layouts/Loading/index";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./common/Theme/theme";
 import { StyledNavLink } from "./StyledApp";
@@ -13,12 +14,30 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectMovieID } from "./features/movies/movie/moviesSlice";
 import { selectPeopleID } from "./features/peoples/people/peopleSlice";
+import { selectMovieDetailsStatus } from "./features/movies/movieDetails/movieDetailsAndCreditsSlice";
+import { selectPeopleDetailsStatus } from "./features/peoples/peopleDetails/peopleDetailsSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchMovies } from "./features/movies/movie/moviesSlice";
+import { fetchPeople } from "./features/peoples/people/peopleSlice";
+import { fetchGenres } from "./features/movies/genresSilce";
+import { fetchConfiguration } from "./features/configurationSlice";
 
 export const App = () => {
   const [placeholderTextMovies, setPlaceholderTextMovies] = useState(true);
-
+  const movieDetailsStatus = useSelector(selectMovieDetailsStatus);
+  const personDetailsStatus = useSelector(selectPeopleDetailsStatus);
   const movieID = useSelector(selectMovieID);
   const personID = useSelector(selectPeopleID);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchConfiguration());
+    dispatch(fetchGenres());
+    dispatch(fetchMovies());
+    dispatch(fetchPeople());
+  }, [dispatch]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -46,13 +65,13 @@ export const App = () => {
             <PeopleContent />
           </Route>
           <Route path={`/movieDetails/${movieID}`}>
-            <MoviesDetails />
+            {movieDetailsStatus === "loading" ? <Loading /> : <MoviesDetails />}
           </Route>
           <Route path={`/personDetails/${personID}`}>
-            <PersonDetails />
+            {personDetailsStatus === "loading" ? <Loading /> : <PersonDetails />}
           </Route>
           <Route exact path="/">
-            <Redirect to="/popular-movies" />
+            <Redirect to={"/popular-movies"} />
           </Route>
         </Switch>
       </HashRouter>
