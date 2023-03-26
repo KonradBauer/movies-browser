ccd8d00af57a1985544c56c195901920b9c47020
Marcin Florek <florekdeveloper@gmail.com>
2023-03-26 18:24:08 +0200

    Search for movies and people, not complete

---
+++ src/features/queryParameters.js
@@ -0,0 +1,24 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+export const useQueryParameter = (key) => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  return searchParams.get(key);
+};
+
+export const useReplaceQueryParameter = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  return ({ baseUrl, key, value }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (value === undefined) {
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key, value);
+    }
+    const newSearch = searchParams.toString();
+    navigate(`${baseUrl}?${newSearch}`);
+  };
+};
--- src/layouts/Contents/index.js
+++ src/layouts/Contents/index.js
@@ -5,15 +5,22 @@ import { selectMovies } from "../../features/movies/movie/moviesSlice";
 import { selectPosterSizes, selectProfileSizes } from "../../features/configurationSlice";
 import { selectPeople } from "../../features/peoples/people/peopleSlice";
 import { Pagination } from "../Pagination/index";
+import { useQueryParameter } from "../../features/queryParameters";
+import searchQueryParamName from "../../features/searchQueryParamName";
 
 export const Content = () => {
   const movies = useSelector(selectMovies);
   const posterSizes = useSelector(selectPosterSizes);
+  const totalResults = useSelector(selectMovies);
+  const query = useQueryParameter(searchQueryParamName);
+  const boxTitle = query
+    ? `Search results for "${query}" (${totalResults})`
+    : "Popular movies";
 
   return (
     <MainBox>
       <Section>
-        <BoxTitle>Popular movies</BoxTitle>
+        <BoxTitle>{boxTitle}Popular movies</BoxTitle>
         <TilesList>
           {movies.map(
             ({
@@ -50,11 +57,16 @@ export const Content = () => {
 export const PeopleContent = () => {
   const people = useSelector(selectPeople);
   const profileSizes = useSelector(selectProfileSizes);
+  const totalResults = useSelector(selectPeople);
+  const query = useQueryParameter(searchQueryParamName);
+  const boxTitle = query
+    ? `Search results for "${query}" (${totalResults})`
+    : "Popular people";
 
   return (
     <MainBox>
       <Section>
-        <BoxTitle>Popular people</BoxTitle>
+        <BoxTitle>{boxTitle}Popular people</BoxTitle>
         <PersonTilesList>
           {people.map(({ id, key, name, profile_path }) => (
             <Tile
