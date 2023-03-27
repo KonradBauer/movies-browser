7fae39c19ac7136928f3b98e1afdd08eebd496cd
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-03-27 17:57:14 +0200

    added property transform to transition

--- src/layouts/Tile/persons/person/styled.js
+++ src/layouts/Tile/persons/person/styled.js
@@ -16,7 +16,7 @@ export const ContainerLink = styled(NavLink)`
       color: ${({ theme }) => theme.color.black};
       background: ${({ theme }) => theme.color.white};
       box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-      transition: 0.5s;
+      transition: transform 0.5s;
 
       @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         padding: 8px;
