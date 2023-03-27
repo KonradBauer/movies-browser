db6f5bfe0f47a4f40ca51e56b5d1d7a9aa44c75c
Konrad Bauer <konradbauer94@gmail.com>
2023-03-27 22:21:44 +0200

    added interaction back to homepage to button in Error component, and styles

--- src/layouts/Error/styled.js
+++ src/layouts/Error/styled.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { ReactComponent as Danger } from "../../common/svg/danger.svg";
 
@@ -8,6 +9,10 @@ export const Wrapper = styled.div`
   margin-top: 80px;
   max-width: 592px;
   max-height: 201px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileS}px) {
+    margin-top: 40px;
+  }
 `;
 
 export const MainText = styled.span`
@@ -24,6 +29,10 @@ export const MainText = styled.span`
     margin: auto;
     margin-top: 25px;
   }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}px) {
+    font-size: 28px;
+  }
 `;
 
 export const SubText = styled.span`
@@ -37,14 +46,19 @@ export const SubText = styled.span`
   width: 426.53px;
   height: 58px;
   color: ${({ theme }) => theme.color.woodsmoke};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}px) {
+    font-size: 16px;
+  }
 `;
 
 export const StyledDanger = styled(Danger)`
   margin: auto;
 `;
 
-export const Button = styled.button`
+export const Button = styled(NavLink)`
   display: flex;
+  text-decoration: none;
   flex-direction: row;
   justify-content: center;
   align-items: center;
@@ -52,7 +66,7 @@ export const Button = styled.button`
   margin-top: 24px;
   padding: 16px 24px;
   gap: 10px;
-  width: 181px;
+  width: 200px;
   height: 51px;
   border-radius: 5px;
   border: none;
@@ -63,4 +77,11 @@ export const Button = styled.button`
   line-height: 19px;
   color: ${({ theme }) => theme.color.white};
   background-color: ${({ theme }) => theme.color.scienceBlue};
+  transition: 0.2s;
+
+  &:hover {
+    box-shadow: 2px -2px 0px #6d93be, -2px 2px 0px #6d93be, 2px 2px 0px #6d93be,
+      -2px -2px 0px #6d93be;
+    cursor: pointer;
+  }
 `;
