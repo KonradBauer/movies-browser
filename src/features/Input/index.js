5d7e79c49ca7d52e20b63b026ea89d592b5565a5
Konrad Bauer <konradbauer94@gmail.com>
2023-03-29 23:48:52 +0200

    added placeholder dependency on navigation

--- src/features/Input/index.js
+++ src/features/Input/index.js
@@ -1,16 +1,18 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
 import { useQueryParameter, useReplaceQueryParameter } from "../queryParameters";
 import searchQueryParamsName from "../searchQueryParamName";
 import { fetchSearchMovies, changeSearchText } from "../movies/searchMoviesSlice";
 import { StyledInput } from "../../layouts/Header/styled";
-import { selectSearchText } from "../movies/searchMoviesSlice";
 
 export default () => {
   const query = useQueryParameter(searchQueryParamsName);
   const replaceQueryParameter = useReplaceQueryParameter();
   const history = useHistory();
   const dispatch = useDispatch();
+  const location = useLocation();
+  const pathname = location.pathname;
+  console.log(pathname);
 
   const onInputChange = ({ target }) => {
     replaceQueryParameter({
@@ -25,6 +27,16 @@ export default () => {
   };
 
   return (
-    <StyledInput placeholder="Search for movies" value={query || ""} onChange={onInputChange} />
+    <StyledInput
+      placeholder={`Search for ${
+        pathname.includes("/popular-movies")
+          ? "movies"
+          : pathname.includes("popular-people")
+          ? "people"
+          : ""
+      }`}
+      value={query || ""}
+      onChange={onInputChange}
+    />
   );
 };
