854c5c1dcd254f4fca1a84c04d7b6403507e3d47
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-03-29 17:47:49 +0200

    added display condition to person details on crew

--- src/layouts/PersonDetails/index.js
+++ src/layouts/PersonDetails/index.js
@@ -57,39 +57,43 @@ export const PersonDetails = () => {
                   )}
               </List>
             </CastContent>
-            <CrewContent>
-              <SubdivTitle>
-                Movies - crew({peopleCredits.crew && peopleCredits.crew.length})
-              </SubdivTitle>
-              <List>
-                {peopleCredits &&
-                  peopleCredits.crew &&
-                  peopleCredits.crew.map(
-                    ({
-                      id,
-                      poster_path,
-                      title,
-                      release_date,
-                      genre_ids,
-                      vote_average,
-                      vote_count,
-                    }) => (
-                      <Tile
-                        movie
-                        posterSizes={posterSizes}
-                        key={id}
-                        id={id}
-                        poster_path={poster_path}
-                        title={title}
-                        release_date={release_date}
-                        genre_ids={genre_ids}
-                        vote_average={vote_average}
-                        vote_count={vote_count}
-                      />
-                    )
-                  )}
-              </List>
-            </CrewContent>
+            {peopleCredits.crew.length !== 0 ? (
+              <CrewContent>
+                <SubdivTitle>
+                  Movies - crew({peopleCredits.crew && peopleCredits.crew.length})
+                </SubdivTitle>
+                <List>
+                  {peopleCredits &&
+                    peopleCredits.crew &&
+                    peopleCredits.crew.map(
+                      ({
+                        id,
+                        poster_path,
+                        title,
+                        release_date,
+                        genre_ids,
+                        vote_average,
+                        vote_count,
+                      }) => (
+                        <Tile
+                          movie
+                          posterSizes={posterSizes}
+                          key={id}
+                          id={id}
+                          poster_path={poster_path}
+                          title={title}
+                          release_date={release_date}
+                          genre_ids={genre_ids}
+                          vote_average={vote_average}
+                          vote_count={vote_count}
+                        />
+                      )
+                    )}
+                </List>
+              </CrewContent>
+            ) : (
+              ""
+            )}
           </Content>
         </>
       )}
