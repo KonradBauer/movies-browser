d6c2ab7b32448f0ea6e8f633121e76758ea26505
Marcin Florek <florekdeveloper@gmail.com>
2023-03-30 20:12:57 +0200

    padding added

--- src/layouts/Search/styled.js
+++ src/layouts/Search/styled.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   max-width: 1368px;
   margin: 0 auto;
-  margin: 56px 16px 0;
+  margin: 56px auto;
+  padding: 0 16px;
 `;
 
 export const SearchResults = styled.div`
