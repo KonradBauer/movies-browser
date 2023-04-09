bba376d07f171a8c7a6f37a436d82da98696377c
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-04-09 15:52:40 +0200

    changed export to default in loading component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 import PersonDetails from "./layouts/PersonDetails/index";
 import MoviesDetails from "./layouts/MoviesDetails/index";
-import { Loading } from "./layouts/Loading/index";
+import Loading from "./layouts/Loading/index";
 import { ThemeProvider } from "styled-components";
 import { Search } from "./layouts/Search/index";
 import { theme } from "./common/Theme/theme";
