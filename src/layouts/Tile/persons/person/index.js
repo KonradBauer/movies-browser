1fda8def4001238983a7b74f9bc40f1c09204ee2
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-04-09 14:53:27 +0200

    reordered imports in tiles with person and person details

--- src/layouts/Tile/persons/person/index.js
+++ src/layouts/Tile/persons/person/index.js
@@ -1,10 +1,12 @@
-import { APIImageUrl } from "../../../../features/getAPI";
-import Information from "../../Information";
-import { ContainerLink, Image } from "./styled";
-import noProfile from "../../../../common/images/no-person.png";
 import { useDispatch } from "react-redux";
-import { getPeopleID } from "../../../../features/peoples/people/peopleSlice";
+
+import Information from "../../Information";
+import noProfile from "../../../../common/images/no-person.png";
+import { APIImageUrl } from "../../../../features/getAPI";
 import { fetchPeopleDetails } from "../../../../features/peoples/peopleDetails/peopleDetailsSlice";
+import { getPeopleID } from "../../../../features/peoples/people/peopleSlice";
+
+import { ContainerLink, Image } from "./styled";
 
 export const PersonTile = ({ profileSizes, id, name, profile_path, character, job }) => {
   const dispatch = useDispatch();
@@ -18,12 +20,8 @@ export const PersonTile = ({ profileSizes, id, name, profile_path, character, jo
     <ContainerLink to={`/popular-people/${id}`} onClick={dispatchPersonDetailsHandler} person>
       <Image
         person
-        source={
-          profile_path
-            ? `${APIImageUrl}/${profileSizes ? profileSizes[1] : ""}${profile_path}`
-            : noProfile
-        }
-        alt=""
+        source={profile_path ? `${APIImageUrl}/${profileSizes && profileSizes[1]}${profile_path}` : noProfile}
+        alt="profile picture"
       />
       <Information person name={name} character={character} job={job} />
     </ContainerLink>
