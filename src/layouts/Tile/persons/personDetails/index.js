a7a13893dde0dd04f223cc5a05942ad791be05e9
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-04-09 14:53:27 +0200

    reordered imports in tiles with person and person details

--- src/layouts/Tile/persons/personDetails/index.js
+++ src/layouts/Tile/persons/personDetails/index.js
@@ -1,18 +1,12 @@
+import Information from "../../Information";
 import Description from "../../Description";
 import Details from "../../Details";
-import Information from "../../Information";
-import { Container, DescriptionContainer, Image } from "./styled";
 import noPerson from "../../../../common/images/no-person.png";
 import { APIImageUrl } from "../../../../features/getAPI";
 
-export const PersonDetailsTile = ({
-  profileSizes,
-  profile_path,
-  name,
-  birthday,
-  place_of_birth,
-  biography,
-}) => {
+import { Container, DescriptionContainer, Image } from "./styled";
+
+export const PersonDetailsTile = ({ profileSizes, profile_path, name, birthday, place_of_birth, biography }) => {
   return (
     <Container personDetails>
       <Image
