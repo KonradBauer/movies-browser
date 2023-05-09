83a3a7153c4b7aee026d9e232d7c3de7f2237d35
Konrad Bauer <konradbauer94@gmail.com>
2023-05-09 20:58:04 +0200

    changed custom scrollbar to default and add always visible to this scrollbar

--- src/common/GlobalStyles.js
+++ src/common/GlobalStyles.js
@@ -3,19 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
-    *::-webkit-scrollbar {
-    width: 16px;
-  }
-
-  *::-webkit-scrollbar-track {
-    background: #ffffff;
-  }
-
-  *::-webkit-scrollbar-thumb {
-    background-color: #151316;
-    border-radius: 10px;
-    border: 2px solid #efebeb;
-  }
+    overflow-y: scroll;
   }
 
   *, ::after, ::before {
@@ -26,7 +14,5 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Poppins', sans-serif;
     margin: 0;
     background: #F5F5FA;
-   
-  
   }
 `;
